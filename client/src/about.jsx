ca04aa2f5fbd1b4f8a455d30ca93f12ac372d4e8
AxelBresa <bresanovichaxel43@gmail.com>
2024-08-24 20:10:40 -0300

    Se agrego el nav en about

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Acerca_nosotros from './components/about/acerca_nosotros'
+import Nav from './components/Navbar/Nav'
 
 function About() {
   return (
     <div>
+        <Nav></Nav>
         <Acerca_nosotros></Acerca_nosotros>
     </div>
   )
