c1810ea4edc26656048e3e5c0eb2c7a721316e77
AxelBresa <bresanovichaxel43@gmail.com>
2024-08-24 20:08:49 -0300

    Se corregio el error de la imagen que se achicaba y los mensajes de  errores en los css

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Nav from './components/Navbar/Nav';
 import Landing_page from './components/Home/Landin_page';
-import Footer from './footer';
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -14,7 +13,6 @@ function Home() {
     <div style={{ margin: '0px', padding: '0px' }}>
        <Nav onSearch={handleSearch} />
        <Landing_page searchTerm={searchTerm} />
-       <Footer></Footer>
     </div>
   );
 }
