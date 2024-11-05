b14c939e42229526d24fa075193d0da860e75fef
AxelBresa <bresanovichaxel43@gmail.com>
2024-11-05 10:58:16 -0300

    se cambia el diseño de cambiar cuenta en cuanto a su posicionamiento y background

--- client/src/components/cuenta/Cambiar_cuenta.jsx
+++ client/src/components/cuenta/Cambiar_cuenta.jsx
@@ -18,7 +18,7 @@ export default function Cambiar_cuenta() {
   }, []);
 
   return (
-    <div className="max-w-sm mx-auto bg-gradient-to-r from-purple-600 to-indigo-700 text-white p-6 rounded-lg shadow-lg">
+    <div className="max-w-sm mx-auto bg-gradient-to-r from-purple-600 to-indigo-700 text-white p-6 rounded-lg shadow-lg spacer">
       <div className="flex flex-col items-center">
         <div className="w-24 h-24 bg-white rounded-full flex items-center justify-center mb-4 shadow-md">
           <img
@@ -33,7 +33,7 @@ export default function Cambiar_cuenta() {
       <div className="mt-4 border-t border-gray-200 pt-2">
         <ul className="space-y-2">
           {userInfo && (
-            <li className="flex items-center space-x-3 hover:bg-purple-500 hover:text-black p-2 rounded-md transition-all duration-200">
+            <li className="flex items-center space-x-3 p-2 rounded-md transition-all duration-200 cuenta">
               <img src={userInfo.fotoUser || "../../images/usuario.jpg"} alt="" className='w-16 h-16 rounded-full object-cover border-2 border-white shadow-sm' />
               <div className="flex flex-col">
                 <div className="text-lg font-semibold">{userInfo.username}</div>
@@ -42,9 +42,9 @@ export default function Cambiar_cuenta() {
             </li>
           )}
         </ul>
-        <ul className="space-y-2">
+        <ul className="space-y-2 ">
           {userInfo && (
-            <li className="flex items-center space-x-3 hover:bg-purple-500 hover:text-black p-2 rounded-md transition-all duration-200">
+            <li className="flex items-center space-x-3  p-2 rounded-md transition-all duration-200 cuenta">
               <img src={userInfo.fotoUser || "../../images/usuario.jpg"} alt="" className='w-16 h-16 rounded-full object-cover border-2 border-white shadow-sm' />
               <div className="flex flex-col">
                 <div className="text-lg font-semibold">{userInfo.username}</div>
@@ -58,7 +58,7 @@ export default function Cambiar_cuenta() {
       <div className="mt-2 border-t border-gray-200 pt-2">
         <ul className="space-y-2">
           <Link to="/registro" className='no-underline'>
-            <li className="flex items-center justify-center space-x-2 hover:bg-purple-500 hover:text-black p-2 rounded-md transition-all duration-200">
+            <li className="flex items-center justify-center space-x-2  p-2 rounded-md transition-all duration-200 cuenta">
               <FaUserFriends className="w-5 h-5" />
               <span className="font-medium">Añadir otra cuenta</span>
             </li>
@@ -69,7 +69,7 @@ export default function Cambiar_cuenta() {
       <div className="mt-2 border-t border-gray-200 pt-2">
         <ul className="space-y-2">
           <Link to="/cerrar_sesion" className='no-underline'>
-            <li className="flex items-center justify-center space-x-2 hover:bg-purple-500 hover:text-black p-2 rounded-md transition-all duration-200">
+            <li className="flex items-center justify-center space-x-2 t-black p-2 rounded-md transition-all duration-200 cuenta">
               <FaSignOutAlt className="w-5 h-5" />
               <span className="font-medium">Cerrar sesión</span>
             </li>
