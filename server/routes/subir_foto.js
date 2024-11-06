33b9eef543dfd90186159de7b2498b15d8094bfc
AxelBresa <bresanovichaxel43@gmail.com>
2024-11-06 19:08:20 -0300

    Se borro la carpeta server del modelo y el servidor-js se cambio a js

---
+++ server/routes/subir_foto.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import {uploadUserImage, updateUserProfile} from '../controllers/upload_controlers.js';
+
+const router = express.Router();
+
+
+router.put('/editar/:id', updateUserProfile)
+
+router.put('/:id', uploadUserImage)
+
+
+export default router;
+
