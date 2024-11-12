c23e453e6d0ffbe32441e0bac8ac39f1fc9125ad
AxelBresa <bresanovichaxel43@gmail.com>
2024-11-12 10:14:40 -0300

    modal ya cambia el estado

--- client/src/context.jsx
+++ client/src/context.jsx
@@ -1,17 +1,22 @@
-// ThemeContext.js
-import { createContext, useState } from 'react';
+// themeContext.js
+import React, { createContext, useState, useContext } from 'react';
 
-export const ThemeContext = createContext();
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false); // Tema blanco por defecto
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
   const toggleTheme = () => {
-    setIsDarkTheme((prevTheme) => !prevTheme); // Alterna entre blanco y negro
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    console.log("thema ", newTheme)
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme); // Persistir el tema en localStorage
   };
 
   return (
-    <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
