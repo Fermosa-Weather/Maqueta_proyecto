812a3824269624f9f3b82e02b0291969f46570a0
AxelBresa <bresanovichaxel43@gmail.com>
2024-11-22 16:14:37 -0300

    limpieza

--- client/src/api/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-// src/api/axiosInstance.js
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:4000/api', // Cambia esto si el backend está en otro puerto o dominio
-});
-
-export default instance;
--- client/src/components/ui/progress.jsx
+++
@@ -1,8 +0,0 @@
-export function Progress({ value, className }) {
-    return (
-      <div className={`w-full bg-gray-200 rounded ${className}`}>
-        <div style={{ width: `${value}%` }} className="bg-blue-600 h-full rounded"></div>
-      </div>
-    );
-  }
-  
