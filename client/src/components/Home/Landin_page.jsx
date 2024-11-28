f86b01352aabb2515f0241bd83369be157eda911
Leonardo <chichelezcano94@hotmail.com.ar>
2024-11-28 13:54:16 -0300

    cambios

--- client/src/components/Home/Landin_page.jsx
+++ client/src/components/Home/Landin_page.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import { useTheme } from '../../contex/themaContext'; // Importa el hook useTheme
 import "../../stilos/inicio.css"; 
 import {Slider} from './slider';
-
+import Ventana_clima from './venta_clima';
+import Clima_manos from './clima_manos';
+import Características_principales from './caracteristica_principales';
 
 
 const Landing_page = ({ searchTerm }) => {
@@ -13,7 +15,9 @@ const Landing_page = ({ searchTerm }) => {
     <div className={`contenedor_landing ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
 
       <Slider />
-    
+      <Ventana_clima />
+      <Clima_manos />
+      <Características_principales />
 
  
     </div>
