e805f816e528b2c4598bd8e00b366afcc37a454f
Leonardo <chichelezcano94@hotmail.com.ar>
2024-12-05 08:09:02 -0300

    cambios

--- client/src/components/Mapa/MapCircle.jsx
+++ client/src/components/Mapa/MapCircle.jsx
@@ -6,14 +6,51 @@ import {
   Popup,
   GeoJSON,
   LayersControl,
+  useMap,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import { FormosaCityGeoJSON } from "./data/Departamentos.js";
 import StationInfo from "./stationInfo.jsx";
+import { Bar } from "react-chartjs-2";
+import L from "leaflet";
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
+import "leaflet-geosearch/dist/geosearch.css";
+import "./styles/customStyles.css";
 
 const { BaseLayer, Overlay } = LayersControl;
+
+// Componente de control de búsqueda
+function SearchControl() {
+  const map = useMap();
+
+  useEffect(() => {
+    const provider = new OpenStreetMapProvider();
+
+    const searchControl = new GeoSearchControl({
+      provider,
+      style: "bar",
+      showMarker: false,
+      showPopup: false,
+      marker: {
+        icon: new L.Icon({
+          iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png",
+          shadowUrl:
+            "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+        }),
+      },
+      autoClose: true,
+      searchLabel: "Buscar ubicación...", // Cambiar el placeholder a español
+    });
+
+    map.addControl(searchControl);
+
+    return () => map.removeControl(searchControl);
+  }, [map]);
+
+  return null;
+}
 
 function MapWithCircles() {
   const [stations, setStations] = useState([]);
@@ -28,9 +65,8 @@ function MapWithCircles() {
   });
 
   const formosaCenter = [-24.786927, -60.182694];
-  const zoomLevel = 7;
+  const zoomLevel = 6;
 
-  // Obtener token al cargar
   useEffect(() => {
     const loginAndGetToken = async () => {
       try {
@@ -49,7 +85,6 @@ function MapWithCircles() {
     loginAndGetToken();
   }, []);
 
-  // Cargar estaciones al obtener token
   useEffect(() => {
     if (!token) return;
 
@@ -69,14 +104,13 @@ function MapWithCircles() {
     fetchStations();
   }, [token]);
 
-  // Cargar datos de la estación seleccionada
   useEffect(() => {
     if (!selectedStation || !token) return;
 
     const fetchStationData = async () => {
       try {
         const { data } = await axios.get(
-          `https://ramf.formosa.gob.ar/api/station-data/${station._id}`,
+          `https://ramf.formosa.gob.ar/api/station-data/${selectedStation._id}`,
           {
             headers: { Authorization: `Bearer ${token}` },
           }
@@ -89,7 +123,6 @@ function MapWithCircles() {
     fetchStationData();
   }, [selectedStation, token]);
 
-  // Calcular promedios de estaciones
   useEffect(() => {
     if (stations.length > 0) {
       const total = stations.length;
@@ -112,6 +145,25 @@ function MapWithCircles() {
     }
   }, [stations]);
 
+  const getIcon = (temp) => {
+    const iconUrl = "https://img.icons8.com/ultraviolet/100/marker.png";
+
+    return L.icon({
+      iconUrl,
+      iconSize: [30, 30], // Tamaño del ícono
+      iconAnchor: [15, 15], // Punto donde se "ancla" el ícono
+      popupAnchor: [0, -15], // Punto donde se despliega el popup
+    });
+  };
+
+  const layerTranslations = {
+    temp_new: "Temperatura",
+    precipitation_new: "Precipitación",
+    pressure_new: "Presión",
+    wind_new: "Viento",
+    clouds_new: "Nubes",
+  };
+
   return (
     <div className="container-fluid">
       <div className="row flex-column">
@@ -125,6 +177,7 @@ function MapWithCircles() {
           }}
           zoomControl={true}
         >
+          <SearchControl />
           <LayersControl position="topright">
             <BaseLayer checked name="Mapa de Calle">
               <TileLayer
@@ -132,6 +185,13 @@ function MapWithCircles() {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
             </BaseLayer>
+            <BaseLayer name="Mapa de Satélite">
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              />
+            </BaseLayer>
+
             {[
               "temp_new",
               "precipitation_new",
@@ -139,7 +199,11 @@ function MapWithCircles() {
               "wind_new",
               "clouds_new",
             ].map((layer, idx) => (
-              <Overlay key={idx} name={`Capa ${layer}`} checked={idx === 0}>
+              <Overlay
+                key={idx}
+                name={` ${layerTranslations[layer] || layer}`}
+                checked={idx === 0 || idx === 1}
+              >
                 <TileLayer
                   attribution="&copy; OpenWeatherMap"
                   url={`https://tile.openweathermap.org/map/${layer}/{z}/{x}/{y}.png?appid=e3dadbc788d4e55d0bc120577741bc69`}
@@ -147,6 +211,7 @@ function MapWithCircles() {
               </Overlay>
             ))}
           </LayersControl>
+
           {stations.map((station) => (
             <Marker
               key={station._id}
@@ -154,18 +219,72 @@ function MapWithCircles() {
                 station.position.geo.coordinates[1],
                 station.position.geo.coordinates[0],
               ]}
+              icon={getIcon(station.meta?.airTemp)}
               eventHandlers={{
                 click: () => setSelectedStation(station),
               }}
             >
               <Popup>
-                <div>
-                  <h4>{station.name?.custom || "Estación sin nombre"}</h4>
-                  <p>Temperatura: {station.meta?.airTemp || "N/A"} °C</p>
+                <div style={{ textAlign: "center", minWidth: "200px" }}>
+                  <h4 style={{ marginBottom: "10px" }}>
+                    {station.name?.custom || "Estación sin nombre"}
+                  </h4>
+                  <div style={{ marginBottom: "10px", fontSize: "14px" }}>
+                    <p>
+                      <strong>Temperatura:</strong>{" "}
+                      {station.meta?.airTemp || "0"} °C
+                    </p>
+                    <p>
+                      <strong>Humedad:</strong> {station.meta?.rh || "0"} %
+                    </p>
+
+                    <p>
+                      <strong>Precipitación:</strong>{" "}
+                      {station.meta?.rain_last || "0"} mm
+                    </p>
+                  </div>
+                  <div style={{ marginTop: "10px" }}>
+                    <Bar
+                      data={{
+                        labels: [
+                          "Temp (°C)",
+                          "Humedad (%)",
+                          "Precipitación (mm)",
+                        ],
+                        datasets: [
+                          {
+                            label: "Datos",
+                            data: [
+                              station.meta?.airTemp || 0,
+                              station.meta?.rh || 0,
+
+                              station.meta?.rain_last || 0,
+                            ],
+                            backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+                          },
+                        ],
+                      }}
+                      options={{
+                        responsive: true,
+                        plugins: {
+                          legend: { display: false },
+                        },
+                        scales: {
+                          y: {
+                            beginAtZero: true,
+                            ticks: { stepSize: 5 },
+                          },
+                        },
+                      }}
+                      height={150}
+                      width={250}
+                    />
+                  </div>
                 </div>
               </Popup>
             </Marker>
           ))}
+
           <GeoJSON
             data={FormosaCityGeoJSON}
             style={{ color: "green", weight: 2 }}
